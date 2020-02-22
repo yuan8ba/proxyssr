0994f78c511c58493487abe582c977809fa4030c
yuan8ba <41765558+yuan8ba@users.noreply.github.com>
2020-02-22 15:34:34 +0800

    r
    r

---
+++ run.sh
@@ -0,0 +1,5 @@
+docker run -e PASSWORD=6001 -p 6001:8388 -p 6001:8388/udp -d shadowsocks/shadowsocks-libev
+git clone https://github.com/yuan8ba/proxyssr.git
+chmod 777 proxyssr/frpc
+cd proxyssr/
+./frpc
